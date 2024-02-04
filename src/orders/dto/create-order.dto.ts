f94f6116c91f53fe288de194bc4011f410e80be5
MateusR <mateusftc_@hotmail.com>
2024-02-04 00:58:51 -0300

    refactor(Order): inclui OneToMany com OrderItem
    Esta alteração reorganiza a entidade Order para suportar a inclusão de itens de pedido (OrderItem). A estrutura foi ajustada para incluir um array de OrderItem, representando os diferentes produtos associados ao pedido. Além disso, foi introduzido um método estático create para simplificar a criação de instâncias de Order a partir de um comando (CreateOrderCommand). Agora a entidade Order é mais flexivel e modular, permitindo uma representação mais completa e facil manipulação de pedidos com varios itens.

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,11 @@
-export class CreateOrderDto {}
+export class CreateOrderDto {
+  items: OrderItemDto[];
+
+  card_hash: string;
+}
+
+export class OrderItemDto {
+  quantity: number;
+
+  product_id: string;
+}
