5f7d959627a8275c4f47576bbf23a7a84e8983c6
MateusR <mateusftc_@hotmail.com>
2024-02-04 16:02:53 -0300

    refactor(DTO): Usando o Class-validator

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,11 +1,34 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+
 export class CreateOrderDto {
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
   items: OrderItemDto[];
 
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   card_hash: string;
 }
 
 export class OrderItemDto {
+  @IsNotEmpty()
+  @IsInt()
+  @IsPositive()
   quantity: number;
 
+  @MaxLength(36)
+  @IsString()
+  @IsPositive()
   product_id: string;
 }
