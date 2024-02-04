e3bf309856f69c7134aec1953b0b7af3f8feba54
MateusR <mateusftc_@hotmail.com>
2024-02-03 23:45:27 -0300

    feat(Order): Entity criado

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,52 @@
-export class Order {}
+import {
+  Column,
+  CreateDateColumn,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Product } from '../../products/entities/product.entity';
+
+export enum OrderStatus {
+  PENDING = 'pending',
+  PAID = 'paid',
+  FAILED = 'failed',
+}
+
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  total: number;
+
+  @Column()
+  client_id: number; //Usuário atenticado
+
+  @Column()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @CreateDateColumn()
+  creted_at: Date;
+}
+
+export class OrderItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'integer' })
+  quantity: number;
+
+  @Column()
+  price: number;
+
+  @ManyToOne(() => Product)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column()
+  product_id: string;
+
+  @ManyToOne(() => Order)
+  order: Order;
+}
